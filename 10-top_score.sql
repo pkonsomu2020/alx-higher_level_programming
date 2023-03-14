772401e7e3546efdb69e4d45341afec28bf34086
pkonsomu2020 <pkonsomu2020@gmail.com>
2023-03-14 04:32:39 -0700

    Introduction to SQL Project

---
+++ 10-top_score.sql
@@ -0,0 +1,3 @@
+-- Script that lists all records in a table
+-- Query to list all records in the table second_table in a database
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ README.md
@@ -0,0 +1,2 @@
+0x0D. SQL - Introduction 
+Introduction to SQL Tasks
