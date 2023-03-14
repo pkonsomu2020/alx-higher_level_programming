1c85be353e9a7596e819be5c45cbb9115f52580a
pkonsomu2020 <113601561+pkonsomu2020@users.noreply.github.com>
2023-03-14 15:26:44 +0300

    Delete 4-first_table.sql

--- 4-first_table.sql
+++
@@ -1,5 +0,0 @@
--- Create a table in the current database
--- Query to create a table called first_table in the current database in mySQL server
-CREATE TABLE IF NOT EXISTS first_table(
-id INT
-name VARCHAR(256));
