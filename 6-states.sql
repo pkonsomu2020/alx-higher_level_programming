6e4661e8e68264534ad741e74d2e142f39927b6a
pkonsomu2020 <113601561+pkonsomu2020@users.noreply.github.com>
2023-03-15 12:31:03 +0300

    Delete 6-states.sql

--- 6-states.sql
+++
@@ -1,6 +0,0 @@
--- Script that creates a database and a table
--- Query to create a database hbtn_0d_usa
-CREATE DATABASE IF NOT EXISTS hbtn_od_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.sates (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	name VARCHAR(256) NOT NULL);
