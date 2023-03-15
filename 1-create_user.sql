c0a6ebc08cf00198c656f7568e8edafda37a9c19
pkonsomu2020 <113601561+pkonsomu2020@users.noreply.github.com>
2023-03-15 15:02:09 +0300

    Delete 1-create_user.sql

--- 1-create_user.sql
+++
@@ -1,4 +0,0 @@
--- Script that creates a user in mySQL server
--- Query that creates the user 'user_0d_1' in mySQL server
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
