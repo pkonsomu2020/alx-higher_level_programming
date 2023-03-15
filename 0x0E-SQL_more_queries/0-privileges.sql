920b6e6d9ff59b4cf779ed1fad85ca42b8aca27b
pkonsomu2020 <pkonsomu2020@gmail.com>
2023-03-15 03:30:38 -0700

    0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Script that lists all privileges of the mySQL users
+-- Query to lists all the privileges of mySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
