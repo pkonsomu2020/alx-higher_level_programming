7e480c2f992e2103826a764490e56153274dd077
pkonsomu2020 <113601561+pkonsomu2020@users.noreply.github.com>
2023-03-15 15:03:59 +0300

    Delete 8-cities_of_california_subquery.sql

--- 8-cities_of_california_subquery.sql
+++
@@ -1,9 +0,0 @@
--- Script that lists all the cities of california registered in the database
--- Query to list all the cities from california
-SELECT id, name
-FROM cities
-WHERE state_id = (
-	SELECT id
-	FROM states
-	WHERE name = "california");
-
