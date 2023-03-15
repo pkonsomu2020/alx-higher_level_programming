f6bc4c65e4c043520563af900fc72b881850a23d
pkonsomu2020 <113601561+pkonsomu2020@users.noreply.github.com>
2023-03-15 15:04:14 +0300

    Delete 9-cities_by_state_join.sql

--- 9-cities_by_state_join.sql
+++
@@ -1,5 +0,0 @@
--- Script that lists all the cities contained in the database
--- Query to join cities and states
-SELECT cities.id, cities.name, states.name
-FROM cities
-JOIN states ON cities.states_id = states_id;
