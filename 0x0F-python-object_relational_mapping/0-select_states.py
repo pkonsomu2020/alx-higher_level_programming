dd7c0077abeeb8cabf1f8e277ef1a68c1fe60440
pkonsomu2020 <pkonsomu2020@gmail.com>
2023-04-19 08:11:25 -0700

    My first commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
 
 import MySQLdb
-import sys
 
 if __name__ == "__main__":
-    # get arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # connect to database
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-    )
+    db = MySQLdb.connect(user="root", passwd="ponsomu756@", db="hbtn_0e_0_usa")
 
     # create cursor object
     cursor = db.cursor()
 
     # execute query
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states")
 
     # fetch all results
-    results = cursor.fetchall()
+    states = cursor.fetchall()
 
     # print results
-    for rows in results:
-        print(rows)
+    print(len(states))
 
     # close database connection
-    db.close()
     cursor.close()
+    db.close()
