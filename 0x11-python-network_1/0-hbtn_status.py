d2209e7e22e10ebe0dbdacdbdefb6ad39e6b5446
pkonsomu2020 <pkonsomu2020@gmail.com>
2023-04-28 06:19:14 -0700

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-# script that fetches this url: - https://alx-intranet.hbtn.io/status
+"""
+script that fetches this url: - https://alx-intranet.hbtn.io/status
+"""
+
 import urllib.request
+
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         body_response = response.read()
         utf8_content = body_response.decode('utf-8')
-        print('Body response:')
-        print(f'\t- type: {type(body_response)}')
-        print(f'\t- content: {body_response}')
-        print(f'\t- utf8 content: {utf8_content}')
+    print('Body response:')
+    print(f'\t- type: {type(body_response)}')
+    print(f'\t- content: {body_response}')
+    print(f'\t- utf8 content: {utf8_content}')
